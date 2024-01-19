5b8b1b57001d1e13e16070dd502a5baedf4e3322
dbarton-2019164 <dbarton-2019164@kinal.edu.gt>
2024-01-19 10:53:24 -0600

    Entrega de calculadora

---
+++ Calculadora/logica.js
@@ -0,0 +1,27 @@
+let valorPantalla = '';
+
+
+
+function limpiarPantalla() {
+    valorPantalla = '';
+    actualizarPantalla();
+}
+
+function agregarCaracter(char) {
+    valorPantalla += char;
+    actualizarPantalla();
+}
+
+
+function actualizarPantalla() {
+    document.getElementById('pantalla').value = valorPantalla;
+}
+function calcular() {
+    try {
+        valorPantalla = eval(valorPantalla).toString();
+        actualizarPantalla();
+    } catch (e) {
+        valorPantalla = 'Error!!!'
+        actualizarPantalla();
+    }
+}
